fa44c525ac9cadd70ad02e532ba1cc59f47f12e1
Ryan <ryanbcwang@gmail.com>
2021-06-06 12:46:16 -0700

    feat(7kyu): new problem

---
+++ Codewars/7kyu/categorizeNewMember.py
@@ -0,0 +1,19 @@
+def open_or_senior(data):
+
+    categories = []
+    
+    for i in range(len(data)):
+
+        if data[i][0] >= 55:
+            if data[i][1] > 7:
+                categories.append('Senior')
+                continue
+        
+        categories.append('Open')
+
+    return categories\
+
+# Nice one-liner:
+
+def openOrSenior2(data):
+    return ["Senior" if age >= 55 and handicap >= 8 else "Open" for (age, handicap) in data]

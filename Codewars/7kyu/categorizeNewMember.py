2670126263e15432c5089e32d8f38fb29b148998
Ryan <ryanbcwang@gmail.com>
2021-06-06 12:48:03 -0700

    refactor(categorizeNewMember.py): efficiency 100

--- Codewars/7kyu/categorizeNewMember.py
+++ Codewars/7kyu/categorizeNewMember.py
@@ -4,10 +4,9 @@ def open_or_senior(data):
     
     for i in range(len(data)):
 
-        if data[i][0] >= 55:
-            if data[i][1] > 7:
-                categories.append('Senior')
-                continue
+        if data[i][0] >= 55 and data[i][1] > 7:
+            categories.append('Senior')
+            continue
         
         categories.append('Open')
 

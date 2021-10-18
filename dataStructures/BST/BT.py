cb79f7d3a57dbd6507e567c77fda121eb337ba3e
Ryan <ryanbcwang@gmail.com>
2021-10-17 20:35:29 -0700

    chorse(BST): traversals

--- dataStructures/BST/BT.py
+++ dataStructures/BST/BT.py
@@ -1,9 +1,64 @@
 class TreeNode:
     def __init__(self, key):
-        self.key = key
-        self.left = None
-        self.right = None
+        self.key, self.left, self.right = key, None, None
+    
+    def height(self):
+        if self is None:
+            return 0
+        return 1 + max(TreeNode.height(self.left), TreeNode.height(self.right))
+    
+    def size(self):
+        if self is None:
+            return 0
+        return 1 + TreeNode.size(self.left) + TreeNode.size(self.right)
 
+    def traverse_in_order(self):
+        if self is None: 
+            return []
+        return (TreeNode.traverse_in_order(self.left) + 
+                [self.key] + 
+                TreeNode.traverse_in_order(self.right))
+    
+    def display_keys(self, space='\t', level=0):
+        # If the node is empty
+        if self is None:
+            print(space*level + '∅')
+            return   
+
+        # If the node is a leaf 
+        if self.left is None and self.right is None:
+            print(space*level + str(self.key))
+            return
+
+        # If the node has children
+        display_keys(self.right, space, level+1)
+        print(space*level + str(self.key))
+        display_keys(self.left,space, level+1)    
+    
+    def to_tuple(self):
+        if self is None:
+            return None
+        if self.left is None and self.right is None:
+            return self.key
+        return TreeNode.to_tuple(self.left),  self.key, TreeNode.to_tuple(self.right)
+    
+    def __str__(self):
+        return "BinaryTree <{}>".format(self.to_tuple())
+    
+    def __repr__(self):
+        return "BinaryTree <{}>".format(self.to_tuple())
+    
+    @staticmethod    
+    def parse_tuple(data):
+        if data is None:
+            node = None
+        elif isinstance(data, tuple) and len(data) == 3:
+            node = TreeNode(data[1])
+            node.left = TreeNode.parse_tuple(data[0])
+            node.right = TreeNode.parse_tuple(data[2])
+        else:
+            node = TreeNode(data)
+        return node
 
 def display_keys(node, space='\t', level=0):
     # print(node.key if node else None, level)
@@ -39,11 +94,6 @@ def tree_to_tuple(node):
         return None
     return (tree_to_tuple(node.left), node.key, tree_to_tuple(node.right))
 
-def traverse_in_order(node):
-    if node is None:
-        return []
-    return traverse_in_order(node.left) + [node.key] + traverse_in_order(node.right)
-
 def traverse_pre_order(node):
     if node is None:
         return []
@@ -54,10 +104,7 @@ def traverse_post_order(node):
         return []
     return traverse_pre_order(node.left) + traverse_pre_order(node.right) + [node.key]
 
-def tree_height(node):
-    if node is None:
-        return 0
-    return 1 + max(tree_height(node.left), tree_height(node.right))
+
 
 tree1 = parse_tuple(((1,3,None), 2, ((None, 3, 4), 5, (6, 7, 8))))
 
